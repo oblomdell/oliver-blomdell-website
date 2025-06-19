5a8a97265434b31c48b326cbb0f254b0c6e3091f
Oliver Blomdell <128600290+oblomdell@users.noreply.github.com>
2025-06-19 12:49:52 +0200

    Lade till diverse element på sidan
    Lade till en knapp för "explore my work" samt element för location och "available for work".

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from "next";
 import { AiFillLinkedin, AiFillGithub, AiFillMail } from "react-icons/ai";
 import ThemeToggle from "@/components/ThemeToggle";
 import Image from "next/image"
-import { BiBuilding } from "react-icons/bi";
+import { BiBuilding, BiChevronDown } from "react-icons/bi";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Oliver Blomdell - Information Architect",
@@ -21,42 +22,26 @@ export default function Home() {
     <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900 transition-all duration-300 font-main">
       <div className="max-w-screen-xl mx-auto">
         <section className="mb-10">
-          <nav className="py-10 mb-12 space-x-2 flex justify-between dark:text-white">
+          <nav className="py-10 sm:mb-12 space-x-2 flex justify-between dark:text-white items-center">
             <h1 className="text-xl font-title">oliver blomdell</h1>
             <ul className="flex items-center">
               <li>
                 <ThemeToggle />
               </li>
               <li>
-                <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="#">Resume</a>
+                <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-4 md:ml-8" href="#">Resume</a>
               </li>
             </ul>
           </nav>
-          <div className="text-center p-10">
+          <div className="relative mx-auto w-60 h-60">
+            <Image src="/images/profilepic.png" layout="fill" objectFit="cover" alt="Profile picture" />
+          </div>
+          <div className="text-center pt-10">
             <h2 className="text-5xl py-2 text-teal-600 dark:text-teal-400 font-medium md:text-6xl">Oliver Blomdell</h2>
             <h3 className="text-2xl font-medium py-2 md:text-3xl">Information Architect.</h3>
-
-            <div className="flex justify-center py-2">
-              <div className="flex sm:flex-row sm:items-center flex-col-reverse items-start gap-3 sm:gap-10">
-                <div className="flex items-center gap-3">
-                  <BiBuilding className="w-5 h-5" />
-                  <h3 className="text-gray-400">Malmö, Sweden</h3>
-                </div>
-
-                <Button variant="ghost" className="hover:bg-black/50 duration-300 hover:text-white rounded-full">
-                  <div className="relative flex text-sm gap-1 items-center justify-center h-3 w-3 rounded-full">
-                    <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-500"></span>
-                    <span className="inline-flex rounded-full h-3 w-3 bg-green-500"></span>
-                  </div>
-                  <p>Available for work</p>
-                </Button>
-              </div>
-
-            </div>
-
-            <p className="text-base py-5 leading-8 text-gray-800 dark:text-gray-200 md:text-xl max-w-lg mx-auto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi sequi pariatur vitae cum laboriosam illum atque rem numquam.</p>
+            {/* <p className="text-pretty tracking-tight py-5 text-gray-800 dark:text-gray-200 md:text-xl max-w-lg mx-auto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi sequi pariatur vitae cum laboriosam illum atque rem numquam.</p> */}
           </div>
-          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
+          <div className="mt-5 text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
             <a
               href="https://www.linkedin.com/in/oliverblomdell/"
               target="_blank"
@@ -83,9 +68,28 @@ export default function Home() {
             >
               <AiFillMail /></a>
           </div>
-          <div className="relative mx-auto w-80 h-80 mt-20">
-            <Image src="/images/profilepic.png" layout="fill" objectFit="cover" alt="Profile picture" />
+
+          <div className="flex justify-center py-2">
+            <div className="flex sm:flex-row sm:items-center flex-col-reverse items-start gap-3 sm:gap-10">
+              <div className="flex items-center gap-2 pl-2">
+                <BiBuilding className="w-5 h-5" />
+                <h3 className="text-gray-400">Malmö, Sweden</h3>
+              </div>
+
+              <Button variant="ghost" className="hover:bg-black/50 duration-300 hover:text-white rounded-full">
+                <div className="relative flex text-sm gap-1 items-center justify-center h-3 w-3 rounded-full">
+                  <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-500"></span>
+                  <span className="inline-flex rounded-full h-3 w-3 bg-green-500"></span>
+                </div>
+                <p>Available for work</p>
+              </Button>
+            </div>
           </div>
+
+          <Link href="#" className="mt-5 mx-auto w-50 border bg-black/30 border-gray-700/40 flex group items-center justify-center p-3 cursor-pointer transition-colors duration-200 ease-linear hover:bg-zinc-800/40 rounded-lg">
+            <span>Explore my work</span>
+            <BiChevronDown className="text-2xl ml-2"></BiChevronDown>
+          </Link>
         </section>
 
         <section>
